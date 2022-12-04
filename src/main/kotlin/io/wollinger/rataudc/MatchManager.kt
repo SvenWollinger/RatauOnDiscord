f45a826133931289cb5f8113aaef18226538f5b8
SvenWollinger <accounts@wollinger.email>
2022-12-04 23:17:19 +0100

    MatchManager boilerplate

---
+++ src/main/kotlin/io/wollinger/rataudc/MatchManager.kt
@@ -0,0 +1,29 @@
+package io.wollinger.rataudc
+
+class Match(val ownerID: Long, var opponentID: Long? = null) {
+    var ownerBoard = arrayOf(
+        intArrayOf(0, 0, 0),
+        intArrayOf(0, 0, 0),
+        intArrayOf(0, 0, 0)
+    )
+    var opponentBoard = arrayOf(
+        intArrayOf(0, 0, 0),
+        intArrayOf(0, 0, 0),
+        intArrayOf(0, 0, 0)
+    )
+
+    fun isWaiting() = opponentID == null
+}
+
+object MatchManager {
+    private val matches = HashMap<Long, Match>()
+
+    fun totalMatches() = matches.size
+    fun waitingUsers(): Int {
+        var w = 0
+        matches.forEach { (_, match) ->
+            if(match.isWaiting()) w++
+        }
+        return w
+    }
+}
