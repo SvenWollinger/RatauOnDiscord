a2b6f6847b65cb3fc5b5cbeb588e3871a45f73ac
SvenWollinger <accounts@wollinger.email>
2022-12-07 11:18:57 +0100

    Add Graphics2D.antialise()

--- src/main/kotlin/io/wollinger/rataudc/Extensions.kt
+++ src/main/kotlin/io/wollinger/rataudc/Extensions.kt
@@ -1,6 +1,8 @@
 package io.wollinger.rataudc
 
 import net.dv8tion.jda.api.utils.FileUpload
+import java.awt.Graphics2D
+import java.awt.RenderingHints
 import java.awt.image.BufferedImage
 import java.io.File
 import javax.imageio.ImageIO
@@ -10,4 +12,9 @@ fun BufferedImage.toFileUpload(): FileUpload {
         ImageIO.write(this, "png", it)
         return FileUpload.fromData(it)
     }
+}
+
+fun Graphics2D.antialise() {
+    this.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+    this.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY)
 }
