50ae92820043066b4e834dea3872db2d4ebd7a40
SvenWollinger <accounts@wollinger.email>
2022-12-13 13:01:42 +0100

    Add contact

--- src/main/kotlin/io/wollinger/rataudc/commands/Help.kt
+++ src/main/kotlin/io/wollinger/rataudc/commands/Help.kt
@@ -1,5 +1,6 @@
 package io.wollinger.rataudc.commands
 
+import io.wollinger.rataudc.Ratau
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.events.interaction.component.ButtonInteractionEvent
 import net.dv8tion.jda.api.interactions.commands.build.Commands
@@ -11,7 +12,8 @@ object Help: ICommand {
     override fun run(event: SlashCommandInteractionEvent) {
         event.reply("You need help ey? Pick a topic here...").addActionRow(
             Button.primary("help-about", "Who are you?"),
-            Button.primary("help-kb", "Knucklebones")
+            Button.primary("help-kb", "Knucklebones"),
+            Button.primary("help-contact", "Contact")
         ).setEphemeral(true).queue()
     }
 
@@ -23,13 +25,22 @@ object Help: ICommand {
                     ... or rather a bot made to mimic him!
                     With me you can play Knucklebones! That's all really.. at least for now.
                     
-                    You can find my inner workings here:
-                    <https://github.com/SvenWollinger/RatauOnDiscord>
+                    You can find my inner workings here: <https://github.com/SvenWollinger/RatauOnDiscord>
                 """.trimIndent()
                 event.reply(message).setEphemeral(true).queue()
             }
             "kb" -> {
                 event.reply("TODO: Edit this").setEphemeral(true).queue()
+            }
+            "contact" -> {
+                val message = """
+                    I am being developed by Sven Wollinger, you can find his website here: <https://wollinger.io>
+                    My Git Repository is here: <https://github.com/SvenWollinger/RatauOnDiscord>
+                    
+                    This bot is being hosted by this fella, please redirect all concerns/questions regarding this hosted instance to them:
+                    ${Ratau.ownerUsername} (<@${Ratau.ownerID}>)
+                """.trimIndent()
+                event.reply(message).setEphemeral(true).queue()
             }
             else -> {
                 event.reply("Bad id: $id. Report this to the dev. Thanks!").setEphemeral(true).queue()
